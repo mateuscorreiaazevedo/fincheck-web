c51ce3f09490bf2e4d65f2b57156ce63cfab3a9c
Mateus Azevedo <mateuscorreiaazevedo@gmail.com>
2025-08-30 02:39:32 -0300

    chore(main): add react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Router } from '@/router';
 
+const queryClient = new QueryClient();
+
 export function App() {
-  return <Router />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Router />
+    </QueryClientProvider>
+  );
 }
